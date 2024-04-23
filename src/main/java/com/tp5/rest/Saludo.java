db7e239a203ea57a5142a3ccf413834112a7cc59
davdenis <denis13dav@gmail.com>
2024-04-23 11:48:26 -0300

    Primer Commit. Endpoints y modelo Producto.

---
+++ src/main/java/com/tp5/rest/Saludo.java
@@ -0,0 +1,3 @@
+package com.tp5.rest;
+
+public record Saludo(long id, String content) { }
---
+++ src/main/java/controller/ProductoController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.Producto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductoController {
+
+    private Producto producto = new Producto();
+    private List<Producto> productos = new ArrayList<>();
+
+    @GetMapping
+    private List<Producto> obtenerProductos() {
+        return productos;
+    }
+
+    @GetMapping("/{id}")
+    public Producto obtenerProductoPorId(@PathVariable Long id) {
+        return producto;
+    }
+
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto) {
+        return producto;
+    }
+
+    @PutMapping("/{id")
+    public Producto actualizarProducto(@PathVariable Long id, @RequestBody Producto producto) {
+        return producto;
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarProducto(@PathVariable Long id) {
+
+    }
+
+}
+
